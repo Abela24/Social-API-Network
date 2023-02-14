fa8247c308af0cdf4f560f270b4b8a8e9ba8e0f0
Abela24 <abelalemu95@gmail.com>
2023-02-14 17:35:52 -0500

    created user schema

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+
+}
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: [validateEmail, 'Please enter a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends:
+        //     ///have to figure out
+        [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+
+        ]
+
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+
+})
+
+// creates User model using schema 
+const User = model('User', UserSchema);
+
+//export User model
+module.exports = User;
+
