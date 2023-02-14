afe757db26f69199f99eb694e74841e59c672aa5
Abela24 <abelalemu95@gmail.com>
2023-02-14 17:15:26 -0500

    creating router

---
+++ routes/Api/thoughtRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const { get } = require('http');
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+
+route('/').get(getThought).post(createThought)
+
+
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+// POST new reactions
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+//DELETE reaction by ID
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+
+module.exports = router;
